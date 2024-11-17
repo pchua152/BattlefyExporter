13e6e57e56177258e161648c5d96c64fbec3e4c9
Paul Chua <115382866+pchua152@users.noreply.github.com>
2024-11-16 21:10:32 -0500

    Comment cleanup

--- battlefy_scraper.py
+++ battlefy_scraper.py
@@ -24,38 +24,32 @@ def get_id(link):
 def export_to_excel(battlefy_link: str):
     
     battlefy_id = get_id(battlefy_link)
-    # try:
-    #     if(battlefy_id):
-    requesturl = f'https://dtmwra1jsgyb0.cloudfront.net/tournaments/{battlefy_id}/teams'
-    htmldata = requests.get(requesturl)
-    data = json.loads(htmldata.content.decode('utf-8'))
-    result = {}
-    for s in data:
-                # Pokepaste link
+    try:
+        if(battlefy_id):
+            requesturl = f'https://dtmwra1jsgyb0.cloudfront.net/tournaments/{battlefy_id}/teams'
+            htmldata = requests.get(requesturl)
+            data = json.loads(htmldata.content.decode('utf-8'))
+            result = {}
+            for s in data:
+                        # Pokepaste link
 
-        result[s['captain']['username']] = ((s['name'], s['customFields'][1]['value']))
+                result[s['captain']['username']] = ((s['name'], s['customFields'][1]['value']))
 
-    with open('teams.csv','w',newline='',encoding = 'utf-8') as csvfile:
-        headers = ['BattlefyName', 'IGN', 'Pokepaste']
-        teamwrite = csv.writer(csvfile)
-        teamwrite.writerow(headers)
-        for x,y in result.items():
-            teamwrite.writerow([x,y])
+            with open('teams.csv','w',newline='',encoding = 'utf-8') as csvfile:
+                headers = ['BattlefyName', 'IGN', 'Pokepaste']
+                teamwrite = csv.writer(csvfile)
+                teamwrite.writerow(headers)
+                for x,y in result.items():
+                    teamwrite.writerow([x,y])
                     
     
-    return "Teams written to teams.csv"
-    # except:
-    #     return "Invalid link"
+        return "Teams written to teams.csv"
+    except:
+        return "Invalid link"
     
 
 def get_team(battlefy_link: str, name):
-    #This will take the person's in game name or battlefy name
-    #https://dtmwra1jsgyb0.cloudfront.net/tournaments/66d5bb3ebfeaa5003f4774c4/teams
-    #https://battlefy.com/victoryroad/victory-road-september-challenge-1/66d5bb3ebfeaa5003f4774c4/info?infoTab=details
-    
     battlefy_id = get_id(battlefy_link)
-    
-    
     try:
         if battlefy_id:
             requesturl = f'https://dtmwra1jsgyb0.cloudfront.net/tournaments/{battlefy_id}/teams'
@@ -63,15 +57,10 @@ def get_team(battlefy_link: str, name):
             data = json.loads(htmldata.content.decode('utf-8'))
             result = []
             for s in data:
-                # Pokepaste link
                 if s['name'] == name:
                     result.append((s['customFields'][1]['value']))
-                
-                
-                
         return result
             
-                
     except:
         return "Invalid link"
 
